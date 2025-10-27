2a1ac8b2dc87d66857f6d3d2d56e9c729bce4155
Rodrigo Baptista <rodrigombaptista@gmail.com>
2025-10-27 13:30:57 +0000

    Altera mensagem da página para "Em Construção"
    - Estiliza para centralizar o texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+
 function Home() {
-  return <h1>Teste5</h1>;
+  useEffect(() => {
+    // remove margens e preenchimentos padrão do body
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: "100dvh", // ocupa a tela inteira
+        display: "flex", // ativa o flexbox
+        justifyContent: "center", // centraliza horizontalmente
+        alignItems: "center", // centraliza verticalmente
+      }}
+    >
+      <h1>Em construção</h1>
+    </div>
+  );
 }
 
 export default Home;
