69dcfb8399f2689fd7c8810f8837370c3d841f62
Rodrigo Baptista <rodrigombaptista@gmail.com>
2025-10-28 14:49:01 +0000

    Add `/api/v1/status` endpoint
    - Teste de integração de status 200 adicionado

---
+++ tests/integration/api/v1/get.test.js
@@ -0,0 +1,5 @@
+test("should return status 200 from '/api/v1/status'", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  expect(response.status).toBe(200);
+});
